a100d4118a671905f9af339071b10339193f6acd
ka7li <100121301+ka7li@users.noreply.github.com>
2022-06-18 17:58:34 +0800

    Add files via upload

---
+++ WindowsFormsApp1/stuji.Designer.cs
@@ -0,0 +1,140 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class stuji
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.stujibenxinxiBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.classDataSet = new WindowsFormsApp1.classDataSet();
+            this.stujibenxinxiTableAdapter = new WindowsFormsApp1.classDataSetTableAdapters.stujibenxinxiTableAdapter();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.stujibenxinxiBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.classDataSet)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4,
+            this.Column5});
+            this.dataGridView1.Location = new System.Drawing.Point(0, 1);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 27;
+            this.dataGridView1.Size = new System.Drawing.Size(799, 448);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // stujibenxinxiBindingSource
+            // 
+            this.stujibenxinxiBindingSource.DataMember = "stujibenxinxi";
+            this.stujibenxinxiBindingSource.DataSource = this.classDataSet;
+            // 
+            // classDataSet
+            // 
+            this.classDataSet.DataSetName = "classDataSet";
+            this.classDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // stujibenxinxiTableAdapter
+            // 
+            this.stujibenxinxiTableAdapter.ClearBeforeFill = true;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "姓名";
+            this.Column1.MinimumWidth = 6;
+            this.Column1.Name = "Column1";
+            this.Column1.Width = 125;
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "学号";
+            this.Column2.MinimumWidth = 6;
+            this.Column2.Name = "Column2";
+            this.Column2.Width = 125;
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "性别";
+            this.Column3.MinimumWidth = 6;
+            this.Column3.Name = "Column3";
+            this.Column3.Width = 125;
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "出生地";
+            this.Column4.MinimumWidth = 6;
+            this.Column4.Name = "Column4";
+            this.Column4.Width = 125;
+            // 
+            // Column5
+            // 
+            this.Column5.HeaderText = "宿舍号";
+            this.Column5.MinimumWidth = 6;
+            this.Column5.Name = "Column5";
+            this.Column5.Width = 125;
+            // 
+            // stuji
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "stuji";
+            this.Text = "stuji";
+            this.Load += new System.EventHandler(this.stuji_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.stujibenxinxiBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.classDataSet)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private classDataSet classDataSet;
+        private System.Windows.Forms.BindingSource stujibenxinxiBindingSource;
+        private classDataSetTableAdapters.stujibenxinxiTableAdapter stujibenxinxiTableAdapter;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
+    }
+}
---
+++ WindowsFormsApp1/stuji.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class stuji : Form
+    {
+        public stuji()
+        {
+            InitializeComponent();
+        }
+
+        private void stuji_Load(object sender, EventArgs e)
+        {
+            table();
+        }
+        public void table()
+        {
+            dataGridView1.Rows.Clear();//清空旧数据
+            stushujuku stu = new stushujuku();
+            string sql = "select * from stujibenxinxi";
+            IDataReader xsxx = stu.read(sql);
+            while (xsxx.Read())//对xsxx进行读行循环
+            {
+                dataGridView1.Rows.Add(xsxx[0].ToString(), xsxx[1].ToString(), xsxx[2].ToString(), xsxx[3].ToString(), xsxx[4].ToString());
+            }
+            xsxx.Close();
+            stu.stushujukuClose();
+
+        }
+    }
+}
